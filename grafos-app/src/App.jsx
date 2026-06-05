893cb8d8e1388c6fe2d3e57fc8863c448d02e609
Vane <vmg190104@gmail.com>
2026-06-04 20:07:47 -0600

    Sección Camino de Euler: teoría, quiz, ejercicios y simulación

--- grafos-app/src/App.jsx
+++ grafos-app/src/App.jsx
@@ -1,28 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Topics from './pages/Topics'
+import Euler from './pages/Euler'
 import Dijkstra from './pages/Dijkstra'
 
 // App.jsx tiene las rutas y la lógica de la app
 
 //? poner las demás páginas cuando las tengan listas
-//? ejemplos:
-//* import Euler from './pages/Euler'
-//* import Dijkstra from './pages/Dijkstra'
+//* import Hamilton from './pages/Hamilton'
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/temas" element={<Topics />} />
-        // <Route path="/dijkstra" element={<Dijkstra />} />
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/temas" element={<Topics />} />
+                <Route path="/euler" element={<Euler />} />
+                <Route path="/dijkstra" element={<Dijkstra />} />
 
-        {/*? aquí poner las rutas cuando las tengan listas 
+                {/*? aquí poner las rutas cuando las tengan listas
           ejemplo: */}
 
-        {/* <Route path="/euler" element={<Euler />} /> */}
-      </Routes>
-    </BrowserRouter>
-  )
+                {/* <Route path="/hamilton" element={<Hamilton />} /> */}
+            </Routes>
+        </BrowserRouter>
+    )
 }
